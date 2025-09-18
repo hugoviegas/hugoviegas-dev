53b81547d5944a3f32585459b29d859702783be9
Hugo Viegas <69760051+hugoviegas@users.noreply.github.com>
2025-09-18 18:54:59 +0100

    feat: Add Vercel Analytics integration and update package dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
-import type { AppProps } from "next/app";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
+
+type AppProps = {
+  Component: React.ComponentType<Record<string, unknown>>;
+  pageProps: Record<string, unknown>;
+};
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+      {/* Vercel Web Analytics collector */}
+      <Analytics />
       {/* Add Speed Insights collector */}
       <SpeedInsights />
     </>
